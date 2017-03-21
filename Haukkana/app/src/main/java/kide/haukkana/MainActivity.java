cfb2f06a0963914757dc86cbb514d2472bf439b8
jamppajoo <mummo94@hotmail.com>
2017-03-21 15:19:12 +0200

    Map fragment now working on MainActivity, Finds user location and places marker on Oulu

--- Haukkana/app/src/main/java/kide/haukkana/MainActivity.java
+++ Haukkana/app/src/main/java/kide/haukkana/MainActivity.java
@@ -1,13 +1,64 @@
 package kide.haukkana;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView2);
+        mapFragment.getMapAsync(this);
+
+
     }
+
+    public void mapsViewButtonClick(View view) {
+
+    }
+
+    public void listViewButtonClick(View view) {
+
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+            // Show rationale and request permission.
+            mMap.setMyLocationEnabled(true);
+
+        }
+        mMap.setMyLocationEnabled(true);
+        // Add a marker in Sydney and move the camera
+        LatLng oulu = new LatLng(65, 24);
+        mMap.addMarker(new MarkerOptions().position(oulu).title("Marker in Oulu"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(oulu));
+    }
+
+
 }
