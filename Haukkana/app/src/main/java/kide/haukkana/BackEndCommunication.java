ec7fe09f71ee297ca079132aee0be6082e31d11f
jamppajoo <mummo94@hotmail.com>
2017-04-03 12:05:37 +0300

    Added Back end communication file

---
+++ Haukkana/app/src/main/java/kide/haukkana/BackEndCommunication.java
@@ -0,0 +1,15 @@
+package kide.haukkana;
+
+/**
+ * Created by Jammu on 3.4.2017.
+ */
+
+public class BackEndCommunication {
+    public BackEndCommunication(){
+
+    }
+    //Example
+    public String returnStoreName(){
+        return "Prisma Limingantulli";
+    }
+}
