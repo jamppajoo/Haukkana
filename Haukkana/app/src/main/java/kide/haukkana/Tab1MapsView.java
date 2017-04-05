b333ef8d37576b6195e8a4cda990215c17c1bd97
jamppajoo <mummo94@hotmail.com>
2017-04-05 11:21:06 +0300

    Location and markers on map is now working

--- Haukkana/app/src/main/java/kide/haukkana/Tab1MapsView.java
+++ Haukkana/app/src/main/java/kide/haukkana/Tab1MapsView.java
@@ -1,11 +1,7 @@
 package kide.haukkana;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -14,67 +10,95 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
 
 /**
  * Created by Jammu on 22.3.2017.
  */
 
-public class Tab1MapsView extends Fragment implements OnMapReadyCallback {
-    private GoogleMap mMap;
+public class Tab1MapsView extends Fragment  {
+    private GoogleMap googleMap;
+    MapView mMapView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1mapsview, container, false);
 
-        //SupportMapFragment mapFragment = (SupportMapFragment) findFragmentById(R.id.mapView);
-        //mapFragment.getMapAsync(this);
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+
+
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+                    // Show rationale and request permission.
+                    mMap.setMyLocationEnabled(true);
+
+                }
+
+                // For dropping a marker at a point on the Map
+                LatLng oulu = new LatLng(65.012360, 25.468160);
+                googleMap.addMarker(new MarkerOptions().position(oulu).title("Marker Title").snippet("Marker Description"));
+
+
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(oulu).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
 
         return rootView;
     }
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-
-
-        } else {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
-            // Show rationale and request permission.
-            mMap.setMyLocationEnabled(true);
-
-        }
-
-        mMap.setMyLocationEnabled(true);
-        // Add a marker in Sydney and move the camera
-        LatLng oulu = new LatLng(65.0126, 25.4715);
-        mMap.addMarker(new MarkerOptions().position(oulu).title("Marker in Oulu"));
-/*
-        LocationManager locMan = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-
-        Criteria crit = new Criteria();
-
-        Location loc = locMan.getLastKnownLocation(locMan.getBestProvider(crit, false));
-
-        CameraPosition camPos = new CameraPosition.Builder()
-
-                .target(new LatLng(loc.getLatitude(), loc.getLongitude()))
-
-                .zoom(12.8f)
-
-                .build();
-
-        CameraUpdate camUpdate = CameraUpdateFactory.newCameraPosition(camPos);
-
-        mMap.moveCamera(camUpdate);
-*/
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
 }
